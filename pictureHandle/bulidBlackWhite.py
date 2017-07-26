def5a887a212bd8149d1c793df4937bf983611c3
Anilople <Anilople@outlook.com>
2017-07-26 23:44:49 +0800

    注意宽和高

--- pictureHandle/bulidBlackWhite.py
+++ pictureHandle/bulidBlackWhite.py
@@ -1,5 +1,6 @@
 # coding=utf-8
 # 别删上边的注释，可能会编码错误
+# 在python相同目录下，需要一个名字为"1.txt"的文本文件和一个名字为"test.jpg"的文件 #
 from PIL import Image, ImageDraw, ImageFont,ImageFilter
 
 fileName='1.txt' # txt文件名字在这里
@@ -13,40 +14,20 @@ print l[0]
 
 name='test.jpg'
 im = Image.open(name) # 打开文件，这里用在原文件上修改的方式来生成图片
+height=len(l) # l=[[0],[0],[0]] 这里高实际上是l的长度，为3，宽是1
+width=max(map(lambda x:len(x),l)) # 取最大的宽
 # # im.size 返回图片的宽和高，一个二元组
-width=len(l)
-height=max(map(lambda x:len(x),l))
-im.size=height,width
-# print size
-# # im.thumbnail((480,270),Image.ANTIALIAS) # 重置图片大小
-# im.thumbnail(size) # 这个也能重置图片大小，size是二元组
+im.size=width,height
 
 print im.size
 draw = ImageDraw.Draw(im) # 在使用draw.point() 之前，先用这个
 
-for i in range(height): 
-    for j in range(width): 
+for i in range(width):
+    for j in range(height): 
         coordinate=(i,j) # 要绘制的点的坐标
-        # coordinate=(width-i,j) # 要绘制的点的坐标
-        
-        color=l[j][i]
+        color=l[j][i] # 颜色实际上是先取高的下标，然后再取宽的下标
         fill=(color,color,color) # 要绘制的点的颜色,fill(R,G,B)
-        # if j<2:fill=(255,0,0)
-        # if i<5 and j<5:fill=(255,255,0)
         draw.point(coordinate,fill) # 将这个点上色
-        if i<2 and j<2:
-            print color
-            draw.point(coordinate,(255,0,0))
-        # t = im.getpixel((i,j)) # 得到这个点的颜色
-        # if white(t):im.putpixel((i,j),(255,255,255))
 
-
-# # print color
-
-# draw.line((0, 0) + im.size, fill=128)
-# draw.line((0, im.size[1], im.size[0], 0), fill=128)
-
-# ImageDraw.Draw.point()
-# print getLine(0,0,10,10)
 im.save('ok.jpg') # 保存图片
 im.show()
