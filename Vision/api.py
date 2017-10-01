c2b2052dd6bd11ddf1a5ca004a1d4f23576d9c27
Anilople <Anilople@outlook.com>
2017-10-01 23:59:16 +0800

    提高了获取图片像素的效率

--- Vision/api.py
+++ Vision/api.py
@@ -3,6 +3,7 @@
 
 from PIL import Image
 from pylab import *
+from numpy import *
 
 imageName = './Vision/empire.jpg' # 图片路径和名称
 
@@ -16,15 +17,33 @@ imageName = './Vision/empire.jpg' # 图片路径和名称
 # read image to array
 im = array(Image.open(imageName))
 
-# plot the image
-imshow(im)
+# # plot the image
+# imshow(im)
 
-# some points
-x = [100,100,400,400]
-y = [200,500,200,500]
+# # some points
+# x = [100,100,400,400]
+# y = [200,500,200,500]
 
-plot(x,y,'r*')
-plot(x[:2],y[:2])
-# axis('off') # 关闭坐标显示
+# plot(x,y,'r*')
+# plot(x[:2],y[:2])
+# # axis('off') # 关闭坐标显示
 
+# show()
+
+# create a new figure
+# figure()
+
+# don't use color
+# gray()
+
+
+# 彩色图片的im.shape为(高，宽，3)
+print im.shape, im.dtype
+
+# im = array(Image.open(imageName).convert('L'),'f') # 加上 'f' 将数值转换为浮点数
+# 灰色图片的im.shape为(高，宽)
+# print im.shape, im.dtype
+print im[0,0]
+print im[0,0,2]
+print im.max()
 show()
---
+++ Vision/getPixelColor(with Array).py
@@ -0,0 +1,28 @@
+#coding=utf-8
+from PIL import Image
+from pylab import *
+
+imageName = 'test.jpg'
+outFileName = 'out.txt'
+imArr = array(Image.open(imageName))
+
+listToString=f=lambda x:reduce((lambda x,y:str(x)+' '+str(y)),x) # 定义一个list到string的函数
+
+outStringList=[] # 使用来一次性写入文件的string list，可以大幅提高写入时间
+imShape = imArr.shape 
+width = imShape[1] # 图片的宽
+high  = imShape[0] # 图片的高
+# print width,high
+outStringList.append(str(width)+' '+str(high)+'\n') # 加入宽和高的信息
+# print outStringList
+f = open(outFileName,'w') # 覆盖写入
+for h in range(high):
+    for w in range(width):
+        # print imArr[i,j]
+        # outStringList.append(str(i)+' '+str(j)+'\n')
+        # print listToString(imArr[i][j])
+        outStringList.append(str(h)+' '+str(w)+' '+listToString(imArr[h][w])+'\n') # 将每行信息加入outStringList中
+        # print outStringList.count()
+f.writelines(outStringList) # 一次性写入outStirngList中的内容
+f.close() # 关闭文件
+# print outStringList
